00d3d33f239ade117eaa5d6a6c989971dd5b1ed5
Henrique <henriquezambon.s@gmail.com>
2024-07-21 22:51:06 -0300

    fix:

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,13 +1,13 @@
-const { Router } = require('express');
-const multer = require('multer');
-const uploadConfig = require('../configs/upload');
+const { Router } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
-const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 const DishesController = require("../controllers/DishesController")
 
-const dishesController = new DishesController();
+const dishesController = new DishesController()
 
-const dishesRoutes = Router();
+const dishesRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 dishesRoutes.use(ensureAuthenticated)
